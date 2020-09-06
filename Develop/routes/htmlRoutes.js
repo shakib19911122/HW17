83b274a56e36b6d9cc1012bdf598fbd00e7ac9e6
shakib <shakib1122@gmail.com>
2020-09-06 22:01:14 +1000

    fix bug

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,16 +1,16 @@
 var path = require("path");
 
-module.exports = function (app){
+module.exports =  (app) => {
     
-    app.get("/exercise", function (req,res){
+    app.get("/exercise",  (req,res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
-    app.get("/", function(req,res){
+    app.get("/", (req,res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    app.get("/stats", function(req, res){
+    app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 
